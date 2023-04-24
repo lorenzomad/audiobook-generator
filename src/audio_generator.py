560f1a9b34aaa6bfa513887fa504aed37d6628ec
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-04-24 10:55:51 +0000

    feat created audio generation item

---
+++ src/audio_generator.py
@@ -0,0 +1,21 @@
+import gtts
+from playsound import playsound
+
+
+class AudioGenerator:
+    #class to generate audio file from text using Google tts
+    def __init__(self):
+        self.tts = None
+
+    def generate_audio(self, text):
+        #creates an audio from the input text
+        self.tts = gtts.gTTS(text)
+        
+
+    def save_audio(self, save_path):
+        #save the file to the path
+        self.tts.save(save_path)
+
+    def play_audio(self, play_path):
+        #plays the audio file saved at play_path
+        playsound(play_path)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from text_extractor import TextExtractor
-
+from audio_generator import AudioGenerator
 
 text_extractor = TextExtractor()
 
 text = text_extractor.read_text("../sample_file/sample.pdf")
 
 print(text)
+
+audio_generator = AudioGenerator()
+
+audio_path = "../audio/sample.mp3"
+audio_generator.generate_audio(text)
+audio_generator.save_audio(audio_path)
+audio_generator.play_audio(audio_path)
